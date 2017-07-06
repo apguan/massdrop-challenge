3573dba31c273225699e745ce2980a39da12b747
Allen Guan <apguan@gmail.com>
2017-07-05 17:36:58 -0700

    set up database functions

--- dbs.js
+++ dbs.js
@@ -16,3 +16,25 @@ var queue = kue.createQueue();
 queue.on( 'error', function(err) {
   console.log( 'Kue Error: ', err );
 });
+
+var fetch = function(job, done) {
+
+}
+
+var createJob = function(url, res) {
+
+}
+
+var getStatus = function(id, res) {
+
+}
+
+queue.process('request', 3, function(job, done) {
+//second argument is the number of jobs that can happen concurrently. Smaller the task, the more that can run concurrently
+
+});
+
+module.exports = {
+  createJob,
+  getStatus
+}
